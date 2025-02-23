baa7cd958ae15d5243e55d374c9fedac6141f4ad
Moshiur-15 <masiurislam28@gmail.com>
2025-02-23 17:10:06 +0600

    LEARN SVELTE

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,25 +1,27 @@
 <!-- src/routes/+layout.svelte -->
 <script>
-    import 'bootstrap/dist/css/bootstrap.min.css'; // Bootstrap CSS
-  </script>
-  
+  import "bootstrap/dist/css/bootstrap.min.css"; // Bootstrap CSS
+</script>
+
+
   <nav class="navbar navbar-expand-lg bg-body-tertiary">
     <div class="container-fluid">
-      <a class="navbar-brand" href="/">Your Logo</a>
-      <div class="collapse navbar-collapse">
-        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-          <li class="nav-item">
-            <a class="nav-link" href="/">Home</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="/about">About</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="/services">Services</a>
-          </li>
-        </ul>
-      </div>
+      <a class="navbar-brand" style="font-style: oblique;" href="/"
+        >LEARN SVELTE</a
+      >
     </div>
+    <ul class="navbar-nav me-auto mb-2 mb-lg-0" style="flex: content;">
+      <li class="nav-item">
+        <a class="nav-link" href="/">Home</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="/about">About</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="/services">Services</a>
+      </li>
+    </ul>
   </nav>
-  
-  <slot />
+
+
+<slot />
